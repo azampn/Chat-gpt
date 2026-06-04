f4ff1443c974addce5057463493810df05ead961
azampn <azamariff36@gmail.com>
2026-06-04 23:43:49 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "newk8svjbuck" # Replace with your actual S3 bucket name
+    bucket = "azamariff2706" 
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
